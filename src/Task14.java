e89d206da6ebb1d2d62b336311e1ca32fa0cefa5
avoronenko <alexandra.voronenko@t-systems.com>
2021-10-06 19:01:16 +0300

    first commit

---
+++ src/Task14.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class Task14 {
+    public static void main(String[] args) {
+
+        long number = 1235;
+        long r = 0;
+        long digit = 1;
+
+
+        while ((digit != r) && (number >= 1)) {
+
+            digit = number % 10;
+            number = number / 10;
+
+            System.out.println(digit);
+
+
+        }
+
+    }
+}
